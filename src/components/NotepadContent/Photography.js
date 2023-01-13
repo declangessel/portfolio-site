68a597acca68af655ded5c24bd8a4fe446b859c9
declangessel <declangessel@gmail.com>
2023-01-13 16:33:04 -0500

    changed link

--- src/components/NotepadContent/Photography.js
+++ src/components/NotepadContent/Photography.js
@@ -8,7 +8,7 @@ function Photography({ content }) {
             <h2>My Photography Portfolio</h2>
 
             <h4>
-                check it out <a href="https://declangessel.com" target="_blank" rel="noopener noreferrer">here</a>
+                check it out <a href="https://declangessel.org" target="_blank" rel="noopener noreferrer">here</a>
             </h4>
             <img src="https://media1.giphy.com/media/lkl6MsCn3iqgZCtTfR/giphy.gif?cid=ecf05e47uroib8yj69zu1cjfpyf6kstzhvw2i5vhjwrftb98&rid=giphy.gif&ct=g" alt="Photo gif" style={{ width: '70%' }}>
             </img>
