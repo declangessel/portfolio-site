906611d888363e7bdd01dd844e21459521ab8278
Declan Gessel <41762496+declangessel@users.noreply.github.com>
2024-03-29 15:16:36 -0400

    Update Photography.js

--- src/components/NotepadContent/Photography.js
+++ src/components/NotepadContent/Photography.js
@@ -8,7 +8,7 @@ function Photography({ content }) {
             <h2>My Photography Portfolio</h2>
 
             <h4>
-                check it out <a href="https://declangessel.org" target="_blank" rel="noopener noreferrer">here</a>
+                check it out <a href="https://declan.photography" target="_blank" rel="noopener noreferrer">here</a>
             </h4>
             <img src="https://media3.giphy.com/media/5Puwg4f1q4Pm9nfE8w/giphy.gif?cid=ecf05e47k80hgf2e9szsx70wnc4pzcdvtbkeojusumealkl1&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="Photo gif" style={{ width: '70%' }}>
             </img>
